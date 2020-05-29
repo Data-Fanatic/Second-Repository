4a67b256f9e9b4446da8852c1dcc24c9b3b2c209
Nabeel Tahir <nabeel.tahir38@mts.ceme.edu.pk>
2020-05-29 05:25:14 +0500

    third attempt

--- scriptAlpha.R
+++ scriptAlpha.R
@@ -3,3 +3,6 @@ print("This file was created within RStudio")
 print("And now it lives on GitHub")
 
 print("seconf attempt on And now it lives on GitHub")
+
+print("third attempt on And now it lives on GitHub")
+
